d1bfb3cec09888769e95a8bb5417d3c4c289200f
unknown <natorvano.work@gmail.com>
2018-10-21 02:57:08 +0300

    board container test

---
+++ src/__tests__/containers/BoardContainer.test.tsx
@@ -0,0 +1,43 @@
+import {
+  mapDispatchToProps,
+  mapStateToProps
+} from "../../application/containers/BoardContainer";
+import { generateEmptyField } from "../../application/logic/generateEmptyField";
+import { IGlobalState } from "../../application/reducers/rootReducer";
+
+export const testGlobalState: IGlobalState = {
+  mines: 10,
+  width: 10,
+  height: 10,
+  field: generateEmptyField(10, 10),
+  gameStartTimestamp: 1000,
+  gameTimeMs: 25000,
+  seed: "random seed",
+  isFinished: false
+};
+
+it("should provide props from state", () => {
+  expect(mapStateToProps(testGlobalState)).toEqual({
+    field: testGlobalState.field
+  });
+});
+
+it("should dispatch correct actions", () => {
+  const dispatch = jest.fn();
+
+  const firstPosition = { row: 0, column: 22 };
+  const secondPosition = { row: 12, column: 17 };
+
+  const handlers = mapDispatchToProps(dispatch);
+  handlers.openCell(firstPosition);
+  handlers.toggleCell(secondPosition);
+
+  expect(dispatch.mock.calls[0][0]).toEqual({
+    type: "OPEN_CELL",
+    payload: firstPosition
+  });
+  expect(dispatch.mock.calls[1][0]).toEqual({
+    type: "TOGGLE_CELL",
+    payload: secondPosition
+  });
+});
